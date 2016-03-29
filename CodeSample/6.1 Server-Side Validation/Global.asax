85eff4e8cc2372de1ebfb98acc442ada8f2f26ad
hanchaodi <hanchaodi@gmail.com>
2016-03-29 20:09:15 +0800

    0

---
+++ CodeSample/6.1 Server-Side Validation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_6._1_Server_Side_Validation.MvcApplication" Language="C#" %>
---
+++ CodeSample/6.1 Server-Side Validation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_6._1_Server_Side_Validation.Startup))]
+namespace _6._1_Server_Side_Validation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
